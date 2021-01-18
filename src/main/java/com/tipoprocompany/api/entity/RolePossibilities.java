b7e0640004323b4e8585a8658f6a44029562f2f5
TipoProProger <31341005+TipoProProger@users.noreply.github.com>
2021-01-18 14:04:51 +0300

    add comment

--- src/main/java/com/tipoprocompany/api/entity/RolePossibilities.java
+++ src/main/java/com/tipoprocompany/api/entity/RolePossibilities.java
@@ -10,18 +10,28 @@ import javax.persistence.Table;
 /**
  *
  * @author michael
+ * Класс связи таблиц ролей и возможностей. Содержит поля: <b>possibilities</b> <b>role</b> 
+ * @version 1.0
  */
 @Entity
 @Table(name = "ROLE_POSSIBILITIES")
 public class RolePossibilities extends PanacheEntity {
 
+    /** Поле связи с таблицей возможностей
+     * @see Possibilities
+     */
     @ManyToOne(optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "possibilities_id")
     public Possibilities possibilities;
+    
+    /** Поле связи с таблицей ролей
+     * @see Role
+     */
     @ManyToOne(optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id")
     public Role role;
 
+    /** Конструктор без параметров*/ 
     public RolePossibilities() {
     }
 
