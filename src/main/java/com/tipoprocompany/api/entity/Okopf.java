39f36500f81dd5a9607f86298409ee964d1a422a
TipoProProger <31341005+TipoProProger@users.noreply.github.com>
2021-01-18 13:45:07 +0300

    add comments

--- src/main/java/com/tipoprocompany/api/entity/Okopf.java
+++ src/main/java/com/tipoprocompany/api/entity/Okopf.java
@@ -12,22 +12,32 @@ import javax.persistence.Table;
 /**
  *
  * @author michael
+ * Класс ОКОПФ содержит поля: <b>number</b> <b>sysName</b> <b>name</b> <b>business</b> 
+ * @version 1.0
  */
 @Entity
 @Table(name = "OKOPF")
 public class Okopf extends PanacheEntity {
 
+    /** Поле для хранения первой цифры ОКОПФ */
     @Column
     public String number;
+    /** Поле системного имени ОКОПФ */
     @Column
     public String sysName;
+    /** Поле имени ОКОПФ на русском*/
     @Column
     public String name;
 
+    /** 
+     * Поле бизнеса. Вид связи один ко многим.
+     * @see Business
+     */
     @OneToMany (mappedBy = "okopfDict", fetch=FetchType.LAZY)
     @JsonIgnore
     public Collection<Business> business;
     
+    /** Конструктор без параметров */
     public Okopf() {
     }
 
