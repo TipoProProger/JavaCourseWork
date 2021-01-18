401ff9cafcc389a1a59304dcc34e4a49d042c9b5
TipoProProger <31341005+TipoProProger@users.noreply.github.com>
2021-01-18 13:49:11 +0300

    add comments

--- src/main/java/com/tipoprocompany/api/entity/Possibilities.java
+++ src/main/java/com/tipoprocompany/api/entity/Possibilities.java
@@ -12,20 +12,28 @@ import javax.persistence.Table;
 /**
  *
  * @author michael
+ * Класс возможностей пользаветелей по ролям. Содержит поля: <b>sysName</b> <b>name</b> <b>rolePossibilities</b> 
+ * @version 1.0
  */
 @Entity
 @Table(name = "POSSIBILITIES")
 public class Possibilities extends PanacheEntity {
 
+    /** Поле системного имени возможности пользователя*/
     @Column(nullable = false)
     public String sysName;
+    /** Поле имени возможности пользователя на русском*/
     @Column(nullable = false)
     public String name;
 
+    /** Поле класса для таблицы связи с ролямию Вид связи один ко многим
+     * @see RolePossibilities
+     */
     @OneToMany (mappedBy="possibilities", fetch=FetchType.LAZY)
     @JsonIgnore
     public Collection<RolePossibilities> rolePossibilities;
     
+    /** Конструктор без параметров*/
     public Possibilities() {
     }
 
